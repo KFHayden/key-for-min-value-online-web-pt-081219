94587a3fec23574d78a0981b7aa9e035febe787c
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 05:35:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,7 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, 
+  name_hash.collect do |name, size|
+    if size > 3
+      return name
 end
