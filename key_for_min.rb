e79cbcc00497cbf6002279422ec01a9edb6694e2
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 05:38:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,10 @@ def key_for_min_value(name_hash)
       return name
     end
   end
+  name_hash.collect do |name, size|
+    if size < 11
+      return name
+    end
   if name_hash = {}
     return nil
   end
