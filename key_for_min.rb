2ece156d8e269de4ba60569b34e8b718e88f7962
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 06:06:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = 0
+  lowest_value = nil
   
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
